1f6d34367a5981300d11bef229912223c2694e6b
Mark Tomlin <Dygear@gmail.com>
2021-09-09 12:35:29 -0400

    Malformed Example.

---
+++ src/main.rs
@@ -0,0 +1,39 @@
+#[macro_use]
+extern crate serde_derive;
+extern crate serde;
+extern crate serde_xml_rs;
+
+use serde_xml_rs::from_reader;
+
+#[derive(Debug, Deserialize)]
+struct Item {
+    pub name: String,
+    pub source: String
+}
+
+#[derive(Debug, Deserialize)]
+struct Project {
+    pub name: String,
+
+    #[serde(rename = "Item", default)]
+    pub items: Vec<Item>
+}
+
+fn main() {
+    let correct = r##"
+        <Project name="my_project">
+            <Item name="hello" source="world.rs" />
+        </Project>
+    "##;
+    let project: Project = from_reader(correct.as_bytes()).unwrap();
+    println!("{:#?}", project);
+
+    let malformed = r##"
+        <Project name="malformed">
+            <malformed name="Hello" source="world.rs />
+            <WeDontClose This>
+        </Project>
+    "##;
+    let messedup: Project = from_reader(malformed.as_bytes()).unwrap();
+    println!("{:#?}", messedup);
+}
